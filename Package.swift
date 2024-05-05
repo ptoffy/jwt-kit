7c56823b083ec4dc10dc7c83189d0fe488682c41
Paul Toffoloni <paultoffoloni@gmail.com>
2024-05-05 13:34:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
         .library(name: "JWTKit", targets: ["JWTKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/fpseverino/swift-pqcrypto.git", branch: "post-quantum"),
-        .package(path: "../swift-certificates"),
+        .package(url: "https://github.com/fpseverino/swift-crypto.git", branch: "post-quantum"),
+        .package(url: "https://github.com/apple/swift-certificates.git", from: "1.0.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
     ],
     targets: [
         .target(
             name: "JWTKit",
             dependencies: [
-                .product(name: "Crypto", package: "swift-pqcrypto"),
-                .product(name: "_CryptoExtras", package: "swift-pqcrypto"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "X509", package: "swift-certificates"),
                 .product(name: "BigInt", package: "BigInt"),
             ],
